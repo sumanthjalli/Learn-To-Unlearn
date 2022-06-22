e2d9906c431c213d1e49ff0efceef16cb5fbc30c
sumanth-jalli <sumanth.jalli@realpage.com>
2022-06-22 16:24:52 +0530

    test file name modified

---
+++ LeetCodeProblems.Test/LongestCommonPrefixTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Test
+{
+    [TestClass]
+    public class LongestCommonPrefixTest
+    {
+        [TestMethod]
+        public void Logest_Common_Prefix_Reture()
+        {
+            //Given
+            string[] givenStrArray = { "abcde", "abc", "abcd" };
+            var problem = new Longest_Common_Prefix();
+            string outPut = "abc";
+
+            //When
+            var actual = problem.LongestCommonPrefix(givenStrArray);
+
+            //Then
+            Assert.AreEqual(outPut,actual);
+        }
+    }
+}
---
+++ LeetCodeProblems/Longest_Common_Prefix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems
+{
+    public  class Longest_Common_Prefix
+    {
+        public string LongestCommonPrefix(string[] inputString)
+        {
+            string prefix = inputString[0];
+            for (int i = 1; i < inputString.Length; i++)
+            {
+                while (inputString[i].IndexOf(prefix) !=0)
+                {
+                    prefix = prefix.Substring(0,prefix.Length -1);
+                    if (prefix =="")
+                    {
+                        return "";
+                    }
+
+                }
+            }
+            return prefix;
+        }
+    }
+}
